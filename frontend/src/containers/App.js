65101d6b8dfb1c170b41c027dafb4a29fe1dc858
Ben Lardie <benlardie@gmail.com>
2020-05-29 16:44:01 -0400

    functionality of app is done and working

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import Nav from '../components/Nav'
 import Articles from '../containers/Articles'
-import Article from "../containers/Article"
-import { Switch, Route } from "react-router-dom";
+import Article from '../containers/Article'
+import Category from '../containers/Category'
+import { Switch, Route } from 'react-router-dom'
 
 export default function App() {
     return (
-        <div className='App'>
+        <div className="App">
             <Nav />
             <Switch>
                 <Route path="/" component={Articles} exact />
                 <Route path="/article/:id" component={Article} exact />
+                <Route path="/category/:id" component={Category} exact />
             </Switch>
         </div>
     )
