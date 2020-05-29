022327f75e8a7f6c98ea99ba2039877d739b290b
Ben Lardie <benlardie@gmail.com>
2020-05-29 16:18:00 -0400

    now can view indivudual articles

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import Nav from '../components/Nav'
 import Articles from '../containers/Articles'
-
+import Article from "../containers/Article"
+import { Switch, Route } from "react-router-dom";
 
 export default function App() {
     return (
         <div className='App'>
             <Nav />
-            <Articles />
+            <Switch>
+                <Route path="/" component={Articles} exact />
+                <Route path="/article/:id" component={Article} exact />
+            </Switch>
         </div>
     )
 }
